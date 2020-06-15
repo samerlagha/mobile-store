9166adc29f34ef9fc636f25c52c3cdad802eb61d
samerlagha <samer_la@mail.ru>
2020-06-15 22:18:58 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import thunk from 'redux-thunk'
 import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {Provider} from 'react-redux'
-
 import createRootReducer from 'reducers'
-import Layout from 'containers/layout'
 import './main.css'
+import routes from 'routes'
 
 const history = createBrowserHistory()
 const middlewares = [thunk, routerMiddleware(history)]
@@ -21,7 +20,7 @@ const store = createStore(
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <Layout />
+            {routes} 
         </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
